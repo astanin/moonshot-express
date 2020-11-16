7076fcb0556450dde9e1b4d2460ead6313061725
Sergey Astanin <s.astanin@gmail.com>
2020-11-17 00:00:25 +0100

    launch and landing animation, new background

--- scenes/MainScene.gd
+++ scenes/MainScene.gd
@@ -4,14 +4,26 @@ extends Node2D
 onready var PinkThing = preload('res://scenes/PinkThing.tscn')
 onready var Things = get_node("Things")
 onready var Rocket = get_node("Rocket")
+onready var AnimationPlayer = get_node("AnimationPlayer")
 onready var LastThing = null
+var RocketStatus = "InSpace" # "InSpace", "", or "Landed"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawn_thing()
+	#spawn_thing()
+	#AnimationPlayer.play("RocketLanding")
+	#RocketStatus = ""
+	pass
 
 
 func _process(delta):
+	if Input.is_key_pressed(KEY_SPACE):
+		if RocketStatus == "Landed":
+			AnimationPlayer.play("RocketLaunch")
+			RocketStatus = ""
+		elif RocketStatus == "InSpace":
+			AnimationPlayer.play("RocketLanding")
+			RocketStatus = ""
 	spawn_thing()
 
 func spawn_thing():
@@ -24,3 +36,9 @@ func spawn_thing():
 		pt.active = true
 		LastThing = pt
 	
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "RocketLanding":
+		RocketStatus = "Landed"
+	elif anim_name == "RocketLaunch":
+		RocketStatus = "InSpace"
